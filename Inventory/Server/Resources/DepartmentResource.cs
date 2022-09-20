ea502c90d39bc7638f7a3306b3ca6075cf53e571
Szymon Gamża <gamza.szymon@gmail.com>
2022-09-20 16:13:15 +0200

    Removed not needed anymore annotations

--- Inventory/Server/Resources/DepartmentResource.cs
+++ Inventory/Server/Resources/DepartmentResource.cs
@@ -2,25 +2,15 @@
 {
     public class DepartmentResource
     {
-        [SwaggerSchema(ReadOnly = true)]
         public int DepartmentId { get; set; }
-        [Required]
         public string Name { get; set; } = string.Empty;
         public string BuildingNumber { get; set; } = string.Empty;
         public string City { get; set; } = string.Empty;
         public string Street { get; set; } = string.Empty;
         public string StreetNumber { get; set; } = string.Empty;
-
-        [SwaggerSchema(ReadOnly = true)]
         public DateTime CreatedDate { get; set; }
-
-        [SwaggerSchema(ReadOnly = true)]
         public string CreatedBy { get; set; } = string.Empty;
-
-        [SwaggerSchema(ReadOnly = true)]
         public DateTime UpdatedDate { get; set; }
-
-        [SwaggerSchema(ReadOnly = true)]
         public string UpdatedBy { get; set; } = string.Empty;
     }
 }

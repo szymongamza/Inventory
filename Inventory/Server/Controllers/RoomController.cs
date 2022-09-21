6599f50e659efe81ccf7d537eac3c1de1c31be8d
Szymon Gamża <gamza.szymon@gmail.com>
2022-09-21 22:41:40 +0200

    Added mapping Room-RoomResource in controller

--- Inventory/Server/Controllers/RoomController.cs
+++ Inventory/Server/Controllers/RoomController.cs
@@ -1,6 +1,9 @@
 ﻿using Inventory.Server.Services.RoomService;
+using Inventory.Server.Resources;
+using AutoMapper;
 using Inventory.Shared;
 using Microsoft.AspNetCore.Mvc;
+using Inventory.Server.Extensions;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,71 +14,79 @@ namespace Inventory.Server.Controllers
     public class RoomController : ControllerBase
     {
         private readonly IRoomService _roomService;
-        public RoomController(IRoomService roomService)
+        private readonly IMapper _mapper;
+
+        public RoomController(IRoomService roomService, IMapper mapper)
         {
             _roomService = roomService;
+            _mapper = mapper;
         }
 
         // GET: api/Room
         [HttpGet]
-        public async Task<ActionResult<ServiceResponse<List<Room>>>> GetRooms()
+        public async Task<ActionResult<ServiceResponse<List<RoomResource>>>> GetAllAsync()
         {
-            var result = await _roomService.GetRooms();
-            return Ok(result);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+            var result = await _roomService.ListAsync();
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var newResult = _mapper.Map<ServiceResponse<List<Room>>, ServiceResponse<List<RoomResource>>>(result);
+            return Ok(newResult);
         }
 
         // GET api/Room/5
         [HttpGet("{roomId}")]
-        public async Task<ActionResult<ServiceResponse<Room>>> GetRoom(int roomId)
+        public async Task<ActionResult<ServiceResponse<RoomResource>>> GetAsync(int roomId)
         {
-            var result = await _roomService.GetRoom(roomId);
-            return Ok(result);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+            var result = await _roomService.FindByIdAsync(roomId);
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var newResult = _mapper.Map<ServiceResponse<Room>, ServiceResponse<RoomResource>>(result);
+            return Ok(newResult);
         }
 
         // POST api/Room
         [HttpPost]
-        public async Task<ActionResult<ServiceResponse<Room>>> CreateRoom(Room room)
+        public async Task<ActionResult<ServiceResponse<RoomResource>>> PostAsync([FromBody] SaveRoomResource resource)
         {
-            var result = await _roomService.CreateRoom(room);
-            if (result.Success == true)
-            {
-                return Ok(result);
-            }
-            else
-            {
-                return NotFound(result);
-            }
-
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+            var room = _mapper.Map<SaveRoomResource, Room>(resource);
+            var result = await _roomService.AddAsync(room);
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var newResult = _mapper.Map<ServiceResponse<Room>, ServiceResponse<RoomResource>>(result);
+            return Ok(newResult);
         }
 
         // PUT api/Room/5
         [HttpPut("{roomId}")]
-        public async Task<ActionResult<ServiceResponse<Room>>> PutRoom(int roomId, [FromBody] Room room)
+        public async Task<ActionResult<ServiceResponse<RoomResource>>> PutAsync(int roomId, [FromBody] SaveRoomResource resource)
         {
-            var result = await _roomService.PutRoom(roomId, room);
-            if (result.Success == true)
-            {
-                return Ok(result);
-            }
-            else
-            {
-                return NotFound(result);
-            }
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+            var room = _mapper.Map<SaveRoomResource, Room>(resource);
+            var result = await _roomService.UpdateAsync(roomId, room);
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var newResult = _mapper.Map<ServiceResponse<Room>, ServiceResponse<RoomResource>>(result);
+            return Ok(newResult);
         }
 
         // DELETE api/Room/5
         [HttpDelete("{roomId}")]
-        public async Task<ActionResult<ServiceResponse<Room>>> DeleteRoom(int roomId)
+        public async Task<ActionResult<ServiceResponse<RoomResource>>> DeleteAsync(int roomId)
         {
-            var result = await _roomService.DeleteRoom(roomId);
-            if (result.Success == true)
-            {
-                return Ok(result);
-            }
-            else
-            {
-                return NotFound(result);
-            }
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+            var result = await _roomService.DeleteAsync(roomId);
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var newResult = _mapper.Map<ServiceResponse<Room>, ServiceResponse<RoomResource>>(result);
+            return Ok(newResult);
         }
     }
 }

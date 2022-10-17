ca5c0ca1fcbb8f6070c0e366c77fc50126b5c248
Szymon Gamża <gamza.szymon@gmail.com>
2022-10-17 15:36:23 +0200

    Added mapping to DeviceController

--- Inventory/Server/Controllers/DeviceController.cs
+++ Inventory/Server/Controllers/DeviceController.cs
@@ -1,6 +1,9 @@
 using Inventory.Server.Services.DeviceService;
+using Inventory.Server.Resources;
+using AutoMapper;
 using Inventory.Shared;
 using Microsoft.AspNetCore.Mvc;
+using Inventory.Server.Extensions;
 
 namespace Inventory.Server.Controllers
 {
@@ -9,67 +12,77 @@ namespace Inventory.Server.Controllers
     public class DeviceController : ControllerBase
     {
         private readonly IDeviceService _deviceService;
-        public DeviceController(IDeviceService deviceService)
+        private readonly IMapper _mapper;
+        public DeviceController(IDeviceService deviceService, IMapper mapper)
         {
             _deviceService = deviceService;
+            _mapper = mapper;
         }
 
-        //GET ALL DEVICES
+        // GET: api/Device
         [HttpGet]
         public async Task<ActionResult<ServiceResponse<List<Device>>>> GetDevices()
         {
-            var result = await _deviceService.GetDevices();
-            return Ok(result);
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+            var result = await _deviceService.ListAsync();
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var newResult = _mapper.Map<ServiceResponse<List<Device>>, ServiceResponse< List < DeviceResource >>>(result);
+            return Ok(newResult);
         }
-        //GET DEVICE WITH ID AS ARGUMENT
-        [HttpGet("{deviceId}")]
-        public async Task<ActionResult<ServiceResponse<Device>>> GetDevice(int deviceId)
-        {
-            var result = await _deviceService.GetDevice(deviceId);
-            return Ok(result);
-        }
-        //UPDATE DEVICE WITH ID & OBJECT AS ARGUMENTS
-        [HttpPut("{deviceId}")]
-        public async Task<ActionResult<ServiceResponse<Device>>> PutDevice(int deviceId, [FromBody] Device device)
-        {
-            var result = await _deviceService.PutDevice(deviceId, device);
-            if (result.Success == true)
-            {
-                return Ok(result);
-            }
-            else
-            {
-                return NotFound(result);
-            }
-        }
-        //CREATE DEVICE WITH OBJECT AS ARGUMENT
-        [HttpPost]
-        public async Task<ActionResult<ServiceResponse<Device>>> CreateDevice(Device device)
-        {
-            var result = await _deviceService.CreateDevice(device);
-            if (result.Success == true)
-            {
-                return Ok(result);
-            }
-            else
-            {
-                return NotFound(result);
-            }
 
-        }
-        //DELETE DEVICE WITH ID AS ARGUMENT
-        [HttpDelete("{deviceId}")]
-        public async Task<ActionResult<ServiceResponse<Device>>> DeleteDevice(int deviceId)
+        // GET api/Device/5
+        [HttpGet("{deviceId}")]
+        public async Task<ActionResult<ServiceResponse<DeviceResource>>> GetAsync(int deviceId)
         {
-            var result = await _deviceService.DeleteDevice(deviceId);
-            if (result.Success == true)
-            {
-                return Ok(result);
-            }
-            else
-            {
-                return NotFound(result);
-            }
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+            var result = await _deviceService.FindByIdAsync(deviceId);
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var newResult = _mapper.Map<ServiceResponse<Device>, ServiceResponse<DeviceResource>>(result);
+            return Ok(newResult);
+        }
+
+        // POST api/Device
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<DeviceResource>>> PostAsync([FromBody] SaveDeviceResource resource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+            var device = _mapper.Map<SaveDeviceResource, Device>(resource);
+            var result = await _deviceService.AddAsync(device);
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var newResult = _mapper.Map<ServiceResponse<Device>, ServiceResponse<DeviceResource>>(result);
+            return Ok(newResult);
+        }
+        // PUT api/Device/5
+        [HttpPut("{deviceId}")]
+        public async Task<ActionResult<ServiceResponse<DeviceResource>>> PutAsync(int deviceId, [FromBody] SaveDeviceResource resource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+            var device = _mapper.Map<SaveDeviceResource, Device>(resource);
+            var result = await _deviceService.UpdateAsync(deviceId, device);
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var newResult = _mapper.Map<ServiceResponse<Device>, ServiceResponse<DeviceResource>>(result);
+            return Ok(newResult);
+        }
+
+        // DELETE api/Device/5
+        [HttpDelete("{departmentId}")]
+        public async Task<ActionResult<ServiceResponse<DeviceResource>>> DeleteAsync(int deviceId)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+            var result = await _deviceService.DeleteAsync(deviceId);
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var newResult = _mapper.Map<ServiceResponse<Device>, ServiceResponse<DeviceResource>>(result);
+            return Ok(newResult);
         }
     }
 }

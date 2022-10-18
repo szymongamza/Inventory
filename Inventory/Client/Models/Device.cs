735c147aec0f375ac15e889117701421192b3699
Szymon Gamża <gamza.szymon@gmail.com>
2022-10-18 15:35:16 +0200

    Started working with Client

--- Inventory/Client/Models/Device.cs
+++ Inventory/Client/Models/Device.cs
@@ -3,11 +3,12 @@ namespace Inventory.Client
 {
     public class Device : IEntity
     {
-        public int Id { get; set; }
+        public int DeviceId { get; set; }
         public string Manufacturer { get; set; } = string.Empty;
         public string Model { get; set; } = string.Empty;
-
         public string SerialNumber { get; set; } = string.Empty;
+        public int RoomId { get; set; }
+        public string QrCode { get; set; } = string.Empty;
         public DateTime CreatedDate { get; set; }
         public string CreatedBy { get; set; } = string.Empty;
         public DateTime UpdatedDate { get; set; }

15220e9abde4b8872dc2a97ea9ea3da708bac283
Szymon Gamża <40543244+szymongamza@users.noreply.github.com>
2022-10-20 23:03:58 +0200

    Delete Inventory/Client directory
    Decided to left it as API only

--- Inventory/Client/Services/IDeviceService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Inventory.Client.Services
-{
-    public interface IDeviceService
-    {
-        List<Device> Devices { get; set; }
-        Task<ServiceResponse<Device>> GetDevice(int deviceId);
-        Task GetDevices();
-    }
-}
